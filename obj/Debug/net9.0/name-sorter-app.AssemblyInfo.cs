edef747b45e324a215279a65e95b64d7c666ad7c
Hasham Habib <hasham.habib@fullextent.com.au>
2025-02-16 23:26:04 +1100

    git ignore file added

--- obj/Debug/net9.0/name-sorter-app.AssemblyInfo.cs
+++ obj/Debug/net9.0/name-sorter-app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("name-sorter-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05086dd97742784a3158d907c364c68a4786957b")]
 [assembly: System.Reflection.AssemblyProductAttribute("name-sorter-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("name-sorter-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
